54c627bf8873bed520f66f94f877262e35bde8eb
Antonio <web.antonio.popa@gmail.com>
2021-02-14 19:35:29 +0100

    Working on mobile responsive home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './common/components/nav-bar/nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
